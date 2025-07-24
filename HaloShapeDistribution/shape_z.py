f2a49590dc3fba43c5c44d510f553bbb2acef8b8
Haimagull <163295927+Haimagull@users.noreply.github.com>
2025-07-24 10:38:10 +0200

    Update shape_z.py

--- HaloShapeDistribution/shape_z.py
+++ HaloShapeDistribution/shape_z.py
@@ -58,9 +58,6 @@ def fof_file_format_experiment(time_slice, run) :
     print("About to read {}".format(file_name))
     with open(file_name, "rb") as in_file:
         data = np.fromfile(in_file, dtype = fof_type)
-    print(data['position_of_deepest_potential'])
-    print(data['position_of_centre_of_mass'])
-    print(data.shape)
     return data
 
 def particle_mass(run, data) :
@@ -82,47 +79,39 @@ def shape(run, data, time_slice, redshift_val) :
     mass = data['mass']*u.Msun
     inertia = data['moment_of_inertia']
     M_part = particle_mass(run, data)
-    a_data = [] #list of a values
-    b_data = [] #list of b values
-    c_data = [] #list of c values
-    eigenvect_data = []
-    output_dir = os.path.join("results", run, "moments") #output directory to save results
+    output_dir = os.path.join("results", run, time_slice) #output directory to save results
     os.makedirs(output_dir, exist_ok=True) #check it exists
-    output_file = os.path.join(output_dir, f"shape_{run}.csv") #output file
+    output_file = os.path.join(output_dir, f"shape_{run}_{time_slice}.csv") #output file
     with open(output_file, mode='w', newline='') as file : #save everything in a csv file
         writer = csv.writer(file)
-        writer.writerow(["Time Slice", "Redshift", "a", "b", "c"])
-    for i,row in enumerate(inertia) :
-        Np_halo = mass[i] / M_part #Nb of part. in halo
-        if Np_halo > 300 :
-            """Construction of a, b, c from inertia matrix
-            We have 6 independent components
-            Diagonal terms : d1=Ixx; d2=Iyy; d3=Izz
-            Triangle terms : t1=Ixy=Iyx; t2=Ixz=Izx; t3=Iyz=Izy
-            + Output is two ratios, first c/a then b/a"""
-            d1, t1, t2, d2, t3, d3 = row #get matrix independent components from file
-            I = np.array([[d1,t1,t2], [t1,d2,t3], [t2,t3,d3]]) #reconstructing inertia matrix
-            eigenval, eigenvect = np.linalg.eigh(I) #get eigen values sorted with a>=b>=c
-            c, b, a = np.sqrt(abs(eigenval))  # 
-            a_data.append(a)
-            b_data.append(b)
-            c_data.append(c)
-            #write to csv
-            writer.writerow([time_slice, redshift_val, a_data, b_data, c_data])
-        else :
-            continue
-    return a_data, b_data, c_data
+        writer.writerow(["line","a", "b", "c"])
+        count = 0
+        for i,row in enumerate(inertia) :
+            Np_halo = mass[i] / M_part #Nb of part. in halo
+            if Np_halo > 300 :
+                """Construction of a, b, c from inertia matrix
+                We have 6 independent components
+                Diagonal terms : d1=Ixx; d2=Iyy; d3=Izz
+                Triangle terms : t1=Ixy=Iyx; t2=Ixz=Izx; t3=Iyz=Izy
+                + Output is two ratios, first c/a then b/a"""
+                d1, t1, t2, d2, t3, d3 = row #get matrix independent components from file
+                I = np.array([[d1,t1,t2], [t1,d2,t3], [t2,t3,d3]]) #reconstructing inertia matrix
+                eigenval, eigenvect = np.linalg.eigh(I) #get eigen values sorted with a>=b>=c
+                c, b, a = np.sqrt(abs(eigenval))
+                count += 1
+                #write to csv
+                writer.writerow([count, a, b, c])
+            else :
+                continue
+        print(f"Time slice {time_slice} in run {run} processed :)")
 
 def get_run_shapes(run) :
     times = [str(i).zfill(5) for i in range(1, 101)]
     redshift_val = read_z_values(run)
     for time_slice,z in zip(times, redshift_val) :
-        data = fof_file_format_experiment(time_slice, data)
+        data = fof_file_format_experiment(time_slice, run)
         shape(run, data, time_slice=time_slice, redshift_val=redshift_val)
 
 # EXECUTION
-runs = ['run???', 'run???'] #replace with wanted runs
-for run in runs :
-    get_run_shapes(run=run)
-
+get_run_shapes(run='run???')
 
